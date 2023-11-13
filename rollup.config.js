5439cb288c44ebc3097dc50dcc5410f80e52d3a0
alekseikurshakov <a.kurshakov@impay-online.com>
2023-11-13 13:08:21 +0700

    Initialize repo deps

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+const typescript = require('rollup-plugin-typescript2')
+const pkg = require('./package.json')
+
+const moduleName = pkg.name.replace(/^\@.*\//, '')
+const banner = `/*!
+${moduleName}.js v${pkg.version}
+${pkg.homepage}
+Released under the ${pkg.license} License.
+*/`
+
+module.exports = {
+  input: './src/index.ts',
+  external: [],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: 'inline',
+      banner,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: 'inline',
+      banner,
+    },
+  ],
+  plugins: [typescript()],
+}
