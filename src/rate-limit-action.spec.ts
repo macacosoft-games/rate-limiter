721a681d794c5817d39558afeb192af237b09e24
alekseikurshakov <a.kurshakov@impay-online.com>
2023-11-28 14:38:11 +0700

    Add implementation for IRateLimitCancellableAction interface

---
+++ src/rate-limit-action.spec.ts
@@ -0,0 +1,52 @@
+import { RateLimitCancellableAction } from './rate-limit-action';
+
+describe('RateLimitCancellableAction', () => {
+  let onCancelMock: jest.Mock;
+  let action: RateLimitCancellableAction;
+
+  beforeEach(() => {
+    onCancelMock = jest.fn().mockResolvedValue(undefined);
+    action = new RateLimitCancellableAction(5, onCancelMock);
+  });
+
+  test('should initialize with correct token count and null dates', () => {
+    expect(action.tokens).toBe(5);
+    expect(action.started).toBeNull();
+    expect(action.performed).toBeNull();
+  });
+
+  test('should set started date', () => {
+    const startDate = new Date();
+    action.startAction(startDate);
+    expect(action.started).toEqual(startDate);
+  });
+
+  test('should not allow started date to be set more than once', () => {
+    const startDate = new Date();
+    action.startAction(startDate);
+
+    const newStartDate = new Date();
+    action.startAction(newStartDate); // Attempt to set a different start date
+    expect(action.started).toEqual(startDate); // The start date should not change
+  });
+
+  test('should set performed date', () => {
+    const performedDate = new Date();
+    action.completeAction(performedDate);
+    expect(action.performed).toEqual(performedDate);
+  });
+
+  test('should not allow performed date to be set more than once', () => {
+    const performedDate = new Date();
+    action.completeAction(performedDate);
+
+    const newPerformedDate = new Date();
+    action.completeAction(newPerformedDate); // Attempt to set a different performed date
+    expect(action.performed).toEqual(performedDate); // The performed date should not change
+  });
+
+  test('should call onCancel function when cancelled', async () => {
+    await action.cancel();
+    expect(onCancelMock).toHaveBeenCalled();
+  });
+});
--- src/rate-limit-action.ts
+++ src/rate-limit-action.ts
@@ -26,3 +26,70 @@ export interface IRateLimitCancellableAction extends IRateLimitAction {
    */
   cancel(): Promise<void>;
 }
+/**
+ * Represents a cancellable rate-limited action.
+ */
+export class RateLimitCancellableAction implements IRateLimitCancellableAction {
+  private _performed: Date | null;
+  private _started: Date | null;
+  readonly tokens: number;
+  private readonly onCancel: (action: RateLimitCancellableAction) => Promise<void>;
+
+  /**
+   * Constructs a new RateLimitCancellableAction.
+   *
+   * @param tokens - The number of tokens required for the action.
+   * @param onCancel - A callback function that is called when the action is cancelled.
+   */
+  constructor(tokens: number, onCancel: (action: RateLimitCancellableAction) => Promise<void>) {
+    this.tokens = tokens;
+    this.onCancel = onCancel;
+    this._started = null;
+    this._performed = null;
+  }
+
+  /**
+   * Gets the date and time when the action started.
+   */
+  public get started(): Date | null {
+    return this._started;
+  }
+
+  /**
+   * Gets the date and time when the action was performed.
+   */
+  public get performed(): Date | null {
+    return this._performed;
+  }
+
+  /**
+   * Sets the start time of the action.
+   *
+   * @param date - The date and time when the action started.
+   */
+  public startAction(date: Date): void {
+    if (!this._started) {
+      this._started = date;
+    }
+  }
+
+  /**
+   * Marks the action as performed.
+   *
+   * @param date - The date and time when the action was performed.
+   */
+  public completeAction(date: Date): void {
+    if (!this._performed) {
+      this._performed = date;
+    }
+  }
+
+  /**
+   * Cancels the action.
+   *
+   * @returns A promise that resolves when the action has been successfully cancelled.
+   */
+  async cancel(): Promise<void> {
+    await this.onCancel(this);
+  }
+}
