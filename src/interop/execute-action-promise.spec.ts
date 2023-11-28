a6c0034b33594241fe93e09a2a2b55f53f9d506d
alekseikurshakov <a.kurshakov@impay-online.com>
2023-11-28 15:01:50 +0700

    Add interop executeActionAsPromise

---
+++ src/interop/execute-action-promise.spec.ts
@@ -0,0 +1,44 @@
+import { executeActionAsPromise } from './execute-action-promise';
+
+describe('executeActionAsPromise', () => {
+  let mockRateLimiter: any;
+  let mockAction: jest.Mock;
+
+  beforeEach(() => {
+    mockRateLimiter = {
+      getTokensLeftAsync: jest.fn(),
+      getActionAsync: jest.fn(),
+      releaseActionAsync: jest.fn(),
+    };
+
+    mockAction = jest.fn();
+  });
+
+  test('executes action when enough tokens are available', async () => {
+    mockRateLimiter.getTokensLeftAsync.mockResolvedValue(5); // Assume 5 tokens are available
+    mockAction.mockResolvedValue('action result');
+
+    const result = await executeActionAsPromise(mockAction, 3, mockRateLimiter);
+
+    expect(mockAction).toHaveBeenCalled();
+    expect(result).toBe('action result');
+  });
+
+  test('throws error when rate limit is exceeded', async () => {
+    mockRateLimiter.getTokensLeftAsync.mockResolvedValue(1); // Only 1 token available
+
+    await expect(executeActionAsPromise(mockAction, 3, mockRateLimiter)).rejects.toThrow('Rate limit exceeded');
+
+    expect(mockAction).not.toHaveBeenCalled();
+  });
+
+  test('throws error when action execution fails', async () => {
+    mockRateLimiter.getTokensLeftAsync.mockResolvedValue(5); // Assume 5 tokens are available
+    const error = new Error('Action failed');
+    mockAction.mockRejectedValue(error);
+
+    await expect(executeActionAsPromise(mockAction, 3, mockRateLimiter)).rejects.toThrow('Action failed');
+
+    expect(mockAction).toHaveBeenCalled();
+  });
+});
---
+++ src/interop/execute-action-promise.ts
@@ -0,0 +1,51 @@
+import { IRateLimiter } from '../rate-limiter';
+import { RateLimitError } from '../error/rate-limit-error';
+import { IRateLimitAction } from '../rate-limit-action';
+
+/**
+ * Executes an action as a promise if there are enough tokens available in the rate limiter.
+ * If not enough tokens are available, it throws an error.
+ *
+ * @param actionAsync - A function that returns a promise representing the action to be executed.
+ * @param actionTokens - The number of tokens required from the rate limiter to execute the action.
+ * @param rateLimiter - An instance of IRateLimiter to manage the rate limiting of actions.
+ * @returns A promise that resolves to the result of the action if it is successfully executed within the rate limits.
+ * @throws An `RateLimitError` if the rate limit is exceeded or if an error occurs during the execution of the action.
+ *
+ * @example
+ * executeActionAsPromise(
+ *   () => fetchData(), // The action to perform
+ *   1,                // Number of tokens required
+ *   myRateLimiter     // The rate limiter instance
+ * ).then(result => {
+ *   console.log('Action executed:', result);
+ * }).catch(error => {
+ *   console.error('Error executing action:', error);
+ * });
+ */
+export async function executeActionAsPromise<T>(
+  actionAsync: () => Promise<T>,
+  actionTokens: number,
+  rateLimiter: IRateLimiter,
+): Promise<T> {
+  // Check if enough tokens are available
+  const tokensLeft = await rateLimiter.getTokensLeftAsync();
+  if (tokensLeft < actionTokens) {
+    throw new RateLimitError('Rate limit exceeded');
+  }
+  let action: IRateLimitAction | null = null;
+  try {
+    // Lock tokens for action executing
+    action = await rateLimiter.getActionAsync(actionTokens);
+
+    // Execute the action and returns result
+    return await actionAsync();
+  } catch (error) {
+    throw RateLimitError.fromUnknownReason(error);
+  } finally {
+    // Handle token release
+    if (action) {
+      await rateLimiter.releaseActionAsync(action);
+    }
+  }
+}
